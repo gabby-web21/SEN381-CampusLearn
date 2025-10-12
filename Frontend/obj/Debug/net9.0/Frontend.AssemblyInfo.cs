a44c7cb2be0ffbcad7c880f29ba1f98a811660d0
Smouskous <mornevanrooyen170@gmail.com>
2025-10-12 14:06:46 +0200

    Dashboard calendar

--- Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad493333f711e65b5633c33624a71772387a0673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba0f98ea480c274103cc11a4f24e3bc3a48d492")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Sen381Backend/obj/Debug/net8.0/Sen381Backend.AssemblyInfo.cs
+++ Sen381Backend/obj/Debug/net8.0/Sen381Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sen381Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad493333f711e65b5633c33624a71772387a0673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba0f98ea480c274103cc11a4f24e3bc3a48d492")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sen381Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sen381Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
